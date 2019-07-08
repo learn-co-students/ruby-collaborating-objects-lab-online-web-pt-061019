c1e5fe9f3b6a310dbf7d23cc1309e6fd97008057
David Foote <david.a.foote@gmail.com>
2019-07-08 19:39:52 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,6 +29,22 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+  
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name = name
+    end
+  end
+  
+  def self.create(name)
+    artist = self.new(name)
+    artist.save
+    artist
   end
 end
