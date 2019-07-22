46b0dfc30d75f4f695598c9c1f22a4ddae61ac92
Huy Do <huyddo40@gmail.com>
2019-07-22 04:43:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,30 +6,33 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    songname = filename.split(" - ")[1]
-    song = self.new(songname)
-    # song.title = filename.split(" - ")[1]
-    # song.name = songname
-    artistname = filename.split(" - ")[0]
+    # songname = filename.split(" - ")[1]
+    # song = self.new(songname)
+    # artistname = filename.split(" - ")[0]
     #
-    if (song.artist.nil?)
-      new_artist = Artist.new(artistname)
-      song.artist = new_artist
-      new_artist.songs << song
-      new_artist.save
-    else
-      song.artist.name = artistname
-    end
-    @@all << song
-
-    # if (self.artist.nil?)
-    #   self.artist = Artist.new(artistname)
+    # if (song.artist.nil?)
+    #   new_artist = Artist.new(artistname)
+    #   song.artist = new_artist
+    #   new_artist.songs << song
+    #   new_artist.save
     # else
-    #   self.artist.name = artistname
+    #   song.artist.name = artistname
     # end
+    # @@all << song
+    # song
 
-
+    song_name = filename.split(" - ")[1]
+    song = Song.new(song_name)
+    artist_object = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    song.artist = artist_object
+    song.artist.add_song(self)
     song
+
+    # artist, title = filename.split(" - ")
+    # self.new(title).tap {|song|
+    #   song.artist = Artist.find_or_create_by_name(aritst)
+    #   song.artist.add_song(song)
+    # }
   end
 
 end
